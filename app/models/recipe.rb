6af972eaf4254f967c385cc09e3d4c878bb0f1c7
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-04 04:52:24 +0000

    [Add]ActiveStorage

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :recipe_tag_relations, dependent: :destroy
   belongs_to :end_user
+  # ActiveStorage導入
+  has_one_attached :image
 end

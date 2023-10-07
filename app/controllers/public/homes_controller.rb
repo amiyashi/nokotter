236b2d32163371c59f0b4c8e4f117ac3600c270f
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-07 03:06:35 +0000

    [Add]topページに保存数多い順でレシピ並べる

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,11 @@
 class Public::HomesController < ApplicationController
   def top
-  end  
+    # Bookmarkテーブルの中にある、recipe_idが重複するレコードを並べ替え
+    # count(recipe_id)したデータをDESC（降順）で並べ替え
+    # Bookmarkテーブルに保存されているrecipe_idが同じものを数える
+    # 指定したカラムの値の配列を、対応するデータ型で返す
+    @ranks = Recipe.find(Bookmark.group(:recipe_id).order('count(recipe_id) DESC').limit(5).pluck(:recipe_id))
+  end
   
   def about
   end

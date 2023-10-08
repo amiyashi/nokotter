02e41fc01c53295f3b222eb8bcc70c7641cd4d66
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-08 04:50:23 +0000

    [Fix]フォローボタンの引数修正

--- app/controllers/public/timeline_controller.rb
+++ app/controllers/public/timeline_controller.rb
@@ -1,9 +1,10 @@
 class Public::TimelineController < ApplicationController
   def index
+    @recipes = Recipe.includes(:customer, :bookmarks, :tags)
     @customer = Customer.find(current_customer.id)
     # フォローユーザー情報取得
     @following_customers = @customer.followings
     # すべてのレシピの中から、自分がフォローしてるユーザーのレシピのみを探して表示
-    @recipes = Recipe.where(customer_id: @following_customers).order("created_at DESC")
+    @timeline = @recipes.where(customer_id: @following_customers).order("created_at DESC")
   end
 end

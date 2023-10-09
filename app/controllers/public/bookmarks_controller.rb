7c17b3b6eaebcf24d13d14f65f105b0fdc67c104
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-09 10:23:04 +0000

    [Fix]ブックマークの非同期通信処理

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -7,14 +7,14 @@ class Public::BookmarksController < ApplicationController
   end
 
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    bookmark = current_customer.bookmarks.new(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    bookmark = current_customer.bookmarks.new(recipe_id: @recipe.id)
     bookmark.save
   end
 
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    bookmark = current_customer.bookmarks.find_by(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    bookmark = current_customer.bookmarks.find_by(recipe_id: @recipe.id)
     bookmark.destroy
   end
 end

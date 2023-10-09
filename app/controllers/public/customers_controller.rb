f3939b1e4b0b2053981a7e29574172f531a09886
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-09 07:06:37 +0000

    [Add]アクセス権限（他ユーザーが会員情報編集できない）

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,7 +20,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
-    customer = Customer.find(params[:id])
+    customer = current_customer
     if customer.update(customer_params)
       redirect_to customer_path(customer)
     else

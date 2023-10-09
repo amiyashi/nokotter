bb2cd00aa399a6dd60332776c287653a95a19dd7
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-09 10:00:18 +0000

    [Fix]マイページレイアウト変更

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -10,13 +10,11 @@ class Public::BookmarksController < ApplicationController
     recipe = Recipe.find(params[:recipe_id])
     bookmark = current_customer.bookmarks.new(recipe_id: recipe.id)
     bookmark.save
-    redirect_to recipe_path(recipe)
   end
 
   def destroy
     recipe = Recipe.find(params[:recipe_id])
     bookmark = current_customer.bookmarks.find_by(recipe_id: recipe.id)
     bookmark.destroy
-    redirect_to recipe_path(recipe)
   end
 end

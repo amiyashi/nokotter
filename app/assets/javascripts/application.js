d939af2a45904114ace60ce9fc6ef1d752a6dce0
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-09 13:12:15 +0000

    [Fix]プロフィールレイアウト修正

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require rails-ujs
+
+// .card {
+//   margin: 0 auto 60px;
+//   max-width: 376px;
+//   height: 370px;
+// }
+// .card-body p {
+//   text-align: left;
+// }

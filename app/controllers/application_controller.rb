7323f4c4d01d65d5ce431f6a428326bf2016fd9d
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-11 01:52:07 +0000

    [Fix]新規登録時、フルネーム➝ニックネームに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_customer!, except: [:top, :about]
+  before_action :authenticate_customer!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   # sign_up時に許可するカラム追記
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :birth_date])
   end
 
 end
--- db/migrate/20231002103658_devise_create_customers.rb
+++ db/migrate/20231002103658_devise_create_customers.rb
@@ -32,10 +32,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string "first_name", null: false
-      t.string "last_name", null: false
-      t.string "first_name_kana", null: false
-      t.string "last_name_kana", null: false
+      t.string "nickname", null: false
       t.date "birth_date", null: false
       t.boolean "is_deleted", default: false, null: false
       t.timestamps null: false

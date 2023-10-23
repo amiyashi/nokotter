7016bc564c6d4942e1759652f0ac78576236c20b
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-23 10:34:14 +0000

    [Add]レシピのシード値

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,11 @@ customer_data = [
   { email: "keizi@example.com", nickname: "けいじさま", password: "123456", image_filename: "sample-user2.jpg" },
   { email: "miduki@example.com", nickname: "みー", password: "123456", image_filename: "sample-user3.jpg" },
   { email: "masato@example.com", nickname: "まっかー", password: "123456", image_filename: "sample-user4.jpg" },
-  { email: "hokuto@example.com", nickname: "ほっくー", password: "123456", image_filename: "sample-user5.jpg" },
-  { email: "syun@example.com", nickname: "おんちゃん", password: "123456" }
+  { email: "hokuto@example.com", nickname: "ほっくー", password: "123456", image_filename: "sample-user5.jpg" }
 ]
 
 customer_data.each do |data|
-  customer = Customer.find_or_create_by!(email: data[:email]) do |c|
+  Customer.find_or_create_by!(email: data[:email]) do |c|
     c.nickname = data[:nickname]
     c.password = data[:password]
     c.birth_date = Faker::Date.birthday(min_age: 18, max_age: 65)
@@ -22,7 +21,12 @@ customer_data.each do |data|
 end
 
 
-tag_names = ["パプリカ", "ピーマン", "茄子", "簡単", "おつまみ", "餃子アレンジ", "鶏もも消費", "あっさり", "お家で中華", "中華料理", "炊き込みご飯", "炊飯器", "炊飯器レシピ", "ズボラレシピ", "和風レシピ", "汁物", "豚汁"]
+tag_names = [
+  "パプリカ", "ピーマン", "茄子", "簡単", "おつまみ",
+  "餃子アレンジ", "鶏もも消費", "あっさり", "お家で中華", "中華料理",
+  "炊き込みご飯", "炊飯器", "炊飯器レシピ", "ズボラレシピ", "和風レシピ",
+  "汁物", "豚汁", "秋", "おやつ", "サラダ",
+]
 
 tag_names.each do |name|
   Tag.find_or_create_by!(name: name)
@@ -34,7 +38,11 @@ recipe_data = [
   { title: "手羽餃子", image_filename: "手羽餃子.jpg", customer_id: 2 },
   { title: "油淋鶏", image_filename: "油淋鶏.jpg", customer_id: 3 },
   { title: "炊き込みご飯", image_filename: "炊き込みご飯.jpg", customer_id: 4 },
-  { title: "豚汁", image_filename: "豚汁.jpg", customer_id: 5 }
+  { title: "豚汁", image_filename: "豚汁.jpg", customer_id: 5 },
+  { title: "おさつバター", image_filename: "おさつバター.jpg", customer_id: 1 },
+  { title: "かぼちゃサラダ", image_filename: "かぼちゃサラダ.jpg", customer_id: 1 },
+  { title: "エリンギ豚巻", image_filename: "エリンギ豚巻.jpg", customer_id: 1 },
+  { title: "塩ちゃんこ鍋", image_filename: "塩ちゃんこ鍋.jpg", customer_id: 1 }
 ]
 
 recipe_data.each do |data|
@@ -51,7 +59,11 @@ tag_recipe_relations = [
   { recipe_id: 2, tag_ids: [5, 6] },
   { recipe_id: 3, tag_ids: [5, 7, 8, 9, 10] },
   { recipe_id: 4, tag_ids: [11, 12, 13, 14, 15] },
-  { recipe_id: 5, tag_ids: [15, 16, 17] }
+  { recipe_id: 5, tag_ids: [15, 16, 17] },
+  { recipe_id: 6, tag_ids: [4, 18, 19] },
+  { recipe_id: 7, tag_ids: [18, 20] },
+  { recipe_id: 8, tag_ids: [4] },
+  { recipe_id: 9, tag_ids: [4] }
 ]
 
 tag_recipe_relations.each do |relation|
@@ -117,7 +129,32 @@ ingredients_data = [
 { name: "顆粒和風だし", recipe_id: 5, amount: "小さじ2" },
 { name: "酒", recipe_id: 5, amount: "大さじ1" },
 { name: "味噌", recipe_id: 5, amount: "大さじ2" },
-{ name: "醤油", recipe_id: 5, amount: "小さじ1" }
+{ name: "醤油", recipe_id: 5, amount: "小さじ1" },
+{ name: "さつまいも", recipe_id: 6, amount: "中1/3本" },
+{ name: "バターまたはマーガリン", recipe_id: 6, amount: "10g" },
+{ name: "砂糖または黒糖", recipe_id: 6, amount: "小さじ1/2" },
+{ name: "かぼちゃ", recipe_id: 7, amount: "1/4個" },
+{ name: "玉ねぎ", recipe_id: 7, amount: "1個" },
+{ name: "塩", recipe_id: 7, amount: "小さじ1/2" },
+{ name: "(A)はちみつ", recipe_id: 7, amount: "大さじ2" },
+{ name: "(A)マヨネーズ", recipe_id: 7, amount: "大さじ5" },
+{ name: "豚バラ肉 (スライス)", recipe_id: 8, amount: "6枚" },
+{ name: "エリンギ", recipe_id: 8, amount: "お好きな量" },
+{ name: "片栗粉", recipe_id: 8, amount: "大さじ1" },
+{ name: "(A)ポン酢", recipe_id: 8, amount: "大さじ3" },
+{ name: "(A)はちみつ", recipe_id: 8, amount: "大さじ1" },
+{ name: "ごま油", recipe_id: 8, amount: "大さじ1" },
+{ name: "豚バラ肉 (薄切り)", recipe_id: 9, amount: "150g" },
+{ name: "好みの食材", recipe_id: 9, amount: "お好きな量" },
+{ name: "水", recipe_id: 9, amount: "600ml" },
+{ name: "酒", recipe_id: 9, amount: "大さじ3" },
+{ name: "みりん", recipe_id: 9, amount: "大さじ2" },
+{ name: "鶏ガラスープの素", recipe_id: 9, amount: "小さじ2" },
+{ name: "にんにくチューブ", recipe_id: 9, amount: "適量" },
+{ name: "すりおろし生姜チューブ", recipe_id: 9, amount: "適量" },
+{ name: "砂糖", recipe_id: 9, amount: "小さじ1" },
+{ name: "塩", recipe_id: 9, amount: "小さじ1/2" },
+{ name: "ごま油", recipe_id: 9, amount: "小さじ2" }
 ]
 
 ingredients_data.each do |data|
@@ -149,7 +186,22 @@ procedures_data = [
 { body: "お肉を入れ、中火でひと煮立ちするまで加熱し、アクを取り除きます。", recipe_id: 5 },
 { body: "和風だしと酒を加え、全体に火が通るまで中火で加熱します。", recipe_id: 5 },
 { body: "火を止めて味噌を溶かし入れ、醤油を入れます。", recipe_id: 5 },
-{ body: "ふたたび中火で加熱し、沸騰直前まで温めたら火から下ろし、器に盛り付けて完成です。", recipe_id: 5 }
+{ body: "ふたたび中火で加熱し、沸騰直前まで温めたら火から下ろし、器に盛り付けて完成です。", recipe_id: 5 },
+{ body: "さつまいもの皮を剥き、1㎝角くらいの棒状に切ります。5分ほど水にさらしてあく抜きします。", recipe_id: 6 },
+{ body: "耐熱容器に入れてラップをかけ、レンジで1分ほど温めます。", recipe_id: 6 },
+{ body: "アルミホイルにおいも、バター、砂糖を入れて、フライパンで中火で包み焼き♪", recipe_id: 6 },
+{ body: "バターが溶けてさつまいもに軽く焦げ目がつくまで待ち、完成です！", recipe_id: 6 },
+{ body: "(下準備)かぼちゃの種とワタをスプーンで取っておきます。", recipe_id: 7 },
+{ body: "玉ねぎを薄切りにし、ボウルに入れて塩をふって、10分ほどおいたら、絞って水気を切ります。", recipe_id: 7 },
+{ body: "かぼちゃの皮をそぎ落とし、耐熱ボウルに入れてラップをし、600Wの電子レンジで5分加熱します。", recipe_id: 7 },
+{ body: "熱いうちにマッシャーで潰し、2と(A)を入れて混ぜ合わせます。お皿に盛り付け、完成！", recipe_id: 7 },
+{ body: "エリンギは石づきを切り落とし、縦6等分に切ります。", recipe_id: 8 },
+{ body: "豚バラ肉に手順1をのせて巻き、全体に片栗粉をまぶします。", recipe_id: 8 },
+{ body: "中火で熱したフライパンにごま油をひき、手順2の巻き終わりを下にして3分焼きます。豚バラ肉に火が通るまで、転がしながら炒めます。", recipe_id: 8 },
+{ body: "全体に焼き色がついたら余分な油をふき取り、(A)を入れ、煮絡めたら火からおろして完成です。", recipe_id: 8 },
+{ body: "食材を好みの大きさに切る", recipe_id: 9 },
+{ body: "鍋にスープの材料を全て入れて中火で煮立てる。具材を加えて中火で煮立てたら蓋をし、野菜が柔らかくなるまで加熱する。", recipe_id: 9 },
+{ body: "ひと煮立ちさせたら、ごま油を加えて完成。", recipe_id: 9 }
 ]
 
 procedures_data.each do |data|

efd2893a188c0ceb197ae21c728232f82602a1aa
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-10-26 10:58:58 +0000

    [Fix]フォロー機能バグ修正

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,6 +2,8 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_customer!
   # フォローするとき
   def create
+    # pp params[:customer_id]
+    # raise
     @customer = Customer.find(params[:customer_id])
     current_customer.follow(params[:customer_id])
   end
@@ -10,8 +12,8 @@ class Public::RelationshipsController < ApplicationController
     @customer = Customer.find(params[:customer_id])
     current_customer.unfollow(params[:customer_id])
   end
-  
-  
+
+
   # # フォロー一覧
   # def follower
   #   customer = Customer.find(params[:customer_id])

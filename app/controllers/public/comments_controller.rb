b697f4d58cb0eac50018f1f7f0a9bcb0ec5e37c2
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-11-01 10:32:52 +0000

    [Add]Natural Language API/ネガティブコメント投稿不可

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,14 +2,17 @@ class Public::CommentsController < ApplicationController
   # before_action :ensure_normal_customer, only: [:create, :destroy]
 
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    @comment = current_customer.comments.new(comment_params)
-    @comment.recipe_id = recipe.id
-    @comment.save
+    @recipe = Recipe.find(params[:recipe_id])
+    comment = current_customer.comments.new(comment_params)
+    comment.recipe_id = @recipe.id
+    score = Language.get_data(comment_params[:content])
+    if score > 0.0
+      comment.save
+    end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
+    comment = Comment.find(params[:id])
     Comment.find(params[:id]).destroy
   end
 
